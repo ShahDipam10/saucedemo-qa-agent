24b8dd36e4e8a539626f0e17130f75ab59006a33
Dipam Shah <dipampshah@gmail.com>
2026-03-17 18:55:52 +0530

    Add pages/inventory_page.py

---
+++ pages/inventory_page.py
@@ -0,0 +1,28 @@
+class InventoryPage:
+    def __init__(self, page):
+        self.page = page
+        self.title = page.locator(".title")
+        self.cart_icon = page.locator(".shopping_cart_link")
+        self.cart_badge = page.locator(".shopping_cart_badge")
+
+    def is_loaded(self) -> bool:
+        return self.title.is_visible()
+
+    def get_title(self) -> str:
+        return self.title.inner_text()
+
+    def add_item_to_cart(self, item_name: str):
+        btn_id = f"[data-test='add-to-cart-{item_name.lower().replace(' ', '-')}']"
+        self.page.locator(btn_id).click()
+
+    def remove_item_from_cart(self, item_name: str):
+        btn_id = f"[data-test='remove-{item_name.lower().replace(' ', '-')}']"
+        self.page.locator(btn_id).click()
+
+    def get_cart_count(self) -> int:
+        if self.cart_badge.is_visible():
+            return int(self.cart_badge.inner_text())
+        return 0
+
+    def go_to_cart(self):
+        self.cart_icon.click()
