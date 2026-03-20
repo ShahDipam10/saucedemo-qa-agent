907b6574dc427829a7ba27ba5f711236b74ad49f
Dipam Shah <dipampshah@gmail.com>
2026-03-20 18:00:54 +0530

    add sorting methods to inventory_page

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -4,6 +4,7 @@ class InventoryPage:
         self.title = page.locator(".title")
         self.cart_icon = page.locator(".shopping_cart_link")
         self.cart_badge = page.locator(".shopping_cart_badge")
+        self.sort_dropdown = page.locator("[data-test='product-sort-container']")
 
     def is_loaded(self) -> bool:
         return self.title.is_visible()
@@ -28,3 +29,16 @@ class InventoryPage:
 
     def go_to_cart(self):
         self.cart_icon.click()
+
+    def get_product_names(self) -> list:
+        return self.page.locator("[data-test='inventory-item-name']").all_inner_texts()
+
+    def get_product_prices(self) -> list:
+        raw = self.page.locator("[data-test='inventory-item-price']").all_inner_texts()
+        return [float(p.replace("$", "")) for p in raw]
+
+    def sort_by(self, option: str):
+        self.sort_dropdown.select_option(option)
+
+    def click_product(self, product_name: str):
+        self.page.locator(f"[data-test='inventory-item-name']:has-text('{product_name}')").click()
