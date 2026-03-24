5b0d0ce88a6686cb5a94c2acbe43e2f0e984edb5
Dipam Shah <dipampshah@gmail.com>
2026-03-24 23:38:16 +0530

    phase1: add tests/test_checkout.py

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -1,10 +1,11 @@
 import pytest
+import os
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 from pages.cart_page import CartPage
 from pages.checkout_page import CheckoutPage
 from utils.test_data import LOGIN_VALID, CHECKOUT_INFO
-import os
+from utils.data_loader import invalid_checkout_info, valid_checkout_info
 
 BASE_URL = os.getenv("BASE_URL", "https://www.saucedemo.com")
 
@@ -13,8 +14,7 @@ class TestCheckout:
 
     @pytest.fixture(autouse=True)
     def login_and_add_item(self, page):
-        login = LoginPage(page)
-        login.login(LOGIN_VALID["username"], LOGIN_VALID["password"])
+        LoginPage(page).login(LOGIN_VALID["username"], LOGIN_VALID["password"])
         inventory = InventoryPage(page)
         inventory.add_item_to_cart("sauce-labs-backpack")
         inventory.go_to_cart()
@@ -26,47 +26,45 @@ class TestCheckout:
         checkout.fill_info(CHECKOUT_INFO["first_name"], CHECKOUT_INFO["last_name"], CHECKOUT_INFO["postal_code"])
         checkout.continue_to_overview()
         checkout.finish_order()
-        assert checkout.is_order_complete(), "Order completion message not shown"
+        assert checkout.is_order_complete()
         assert "Thank you" in checkout.get_success_message()
 
-    def test_checkout_missing_first_name(self, page):
+    @pytest.mark.parametrize("first, last, postal, expected_error", invalid_checkout_info())
+    def test_checkout_validation_errors(self, page, first, last, postal, expected_error):
+        """Each missing field combination should show the correct error."""
         cart = CartPage(page)
         checkout = CheckoutPage(page)
         cart.proceed_to_checkout()
-        checkout.fill_info("", CHECKOUT_INFO["last_name"], CHECKOUT_INFO["postal_code"])
+        checkout.fill_info(first, last, postal)
         checkout.continue_to_overview()
-        assert "First Name is required" in checkout.get_error_message()
+        assert expected_error in checkout.get_error_message()
 
-    def test_checkout_missing_postal_code(self, page):
+    @pytest.mark.parametrize("first, last, postal", valid_checkout_info())
+    def test_checkout_succeeds_with_valid_data(self, page, first, last, postal):
+        """Checkout should complete successfully for all valid info combinations."""
         cart = CartPage(page)
         checkout = CheckoutPage(page)
         cart.proceed_to_checkout()
-        checkout.fill_info(CHECKOUT_INFO["first_name"], CHECKOUT_INFO["last_name"], "")
+        checkout.fill_info(first, last, postal)
         checkout.continue_to_overview()
-        assert "Postal Code is required" in checkout.get_error_message()
+        checkout.finish_order()
+        assert checkout.is_order_complete()
 
     def test_checkout_overview_lists_items_and_calculates_total(self, page):
-        # fixture already added backpack + went to cart
-        # go back to inventory to add a second item
         page.goto(f"{BASE_URL}/inventory.html")
         inventory = InventoryPage(page)
         cart = CartPage(page)
         checkout = CheckoutPage(page)
-
         inventory.add_item_to_cart("sauce-labs-bike-light")
         inventory.go_to_cart()
-
         cart.proceed_to_checkout()
         checkout.fill_info(CHECKOUT_INFO["first_name"], CHECKOUT_INFO["last_name"], CHECKOUT_INFO["postal_code"])
         checkout.continue_to_overview()
-
         names = checkout.get_overview_item_names()
         assert "Sauce Labs Backpack" in names
         assert "Sauce Labs Bike Light" in names
-
         item_total = checkout.get_item_total()
         assert item_total > 0
-
         tax = checkout.get_tax_amount()
         total = checkout.get_total()
         assert total == pytest.approx(item_total + tax, rel=1e-2)
